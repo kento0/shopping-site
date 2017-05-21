be9d8a38430c3129cbb326207c31927d46a7b8e8
Kento Kumazawa <kecokeco.02@gmail.com>
2017-05-21 11:08:42 +0000

    second

--- test-carousel.js
+++ test-carousel.js
@@ -8,4 +8,17 @@ $(window).load(function(){
             autoplay: true
         }
     );
+
+$(window).load(function(){
+  $(".pro_img_three img").click(function(){
+    var img_src = $(this).attr("src");
+    $(".pro_img img").attr("src", img_src);
+  });
 });
+
+$(document).ready(function(){
+  $("#sub_navi_btm img").click(function(){
+    $(".sp_menu_toggle").slideToggle();
+  });
+});
+    
